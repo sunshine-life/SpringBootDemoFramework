191866e44283f50ad13426e224bf5ac9598847d1
life <lf@jielicredit.com>
2016-09-28 15:46:46 +0800

    删除非必要文件

--- target/classes/check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-APP_NAME=spring-boot-sample-mybatis-0.0.1-SNAPSHOT.jar
-
-tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
-if [ ${tpid} ]; then
-        echo 'App is running.'
-else
-        echo 'App is NOT running.'
-fi
